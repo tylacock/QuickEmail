c4b8196c0e40b1ae407b2c6090d2600c1275a0ab
tylacock <tylacock@gmail.com>
2013-10-20 12:30:33 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,10 @@
+### Quick Email
+
+A small little app that quickly emails a list of contacts or just an individual.
+
+Currently, it grabs the contents of the pasteboard and asks permission to paste it into the text box.
+
+Still need to add:
+* E-mail functionality :)
+* Creating contact lists
+* Selecting Contact lists
--- textTest/ViewController.m
+++ textTest/ViewController.m
@@ -9,15 +9,37 @@
 #import "ViewController.h"
 
 @interface ViewController ()
-
+@property UIPasteboard *pasteboard;
 @end
 
 @implementation ViewController
 
 - (void)viewDidLoad
 {
+    [self.textView becomeFirstResponder];
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+    
+    self.pasteboard = [UIPasteboard generalPasteboard];
+    if (self.pasteboard) {
+        UIAlertView *message = [[UIAlertView alloc] initWithTitle:@"Paste?"
+                                                          message:@"Would you like to paste your text?"
+                                                         delegate:self
+                                                cancelButtonTitle:@"Yes"
+                                                otherButtonTitles:@"No", nil];
+        [message show];
+        
+    }
+    
+    
+}
+
+- (void)alertView:(UIAlertView *)alertView clickedButtonAtIndex:(NSInteger)buttonIndex
+{
+    NSString *title = [alertView buttonTitleAtIndex:buttonIndex];
+    
+    if ([title isEqualToString:@"Yes"]) {
+        self.textView.text = self.pasteboard.string;
+    }
 }
 
 - (void)didReceiveMemoryWarning
@@ -25,5 +47,6 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
 
 @end
