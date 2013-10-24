ffeaf364bf326c43b1d9c9c4a6b3b4bc19b0884d
tylacock <tylacock@gmail.com>
2013-10-23 22:48:35 -0400

    Started About page

---
+++ textTest/AboutViewController.h
@@ -0,0 +1,13 @@
+//
+//  AboutViewController.h
+//  textTest
+//
+//  Created by Ty Lacock on 10/23/13.
+//  Copyright (c) 2013 TWL. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AboutViewController : UIViewController
+
+@end
